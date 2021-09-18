28445c66b6ef166736af84656e024db4d82ccae9
SRINIVASA RAO MARASU <andeject@gmail.com>
2021-09-18 17:29:28 +0530

    firstcomm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myangularapp';
+  title = 'My Kart App';
+  date = '23 Sep 21';
 }
